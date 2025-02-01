cf9e822256e632a07bc54ab2936a557cb90483d6
Gabriel <gdblombardi@gmail.com>
2025-02-01 12:46:44 -0300

    version v0

--- automeet_ai.py
+++ automeet_ai.py
@@ -13,24 +13,34 @@ st.title('AutoMeet AI')
 
 uploaded_file = st.file_uploader("Choose a file", accept_multiple_files=False, type=['mp4'])
 
-mp4_filename = uploaded_file.name
-mp3_filename = uuid.uuid4().hex + ".mp3"
 
-st.text(mp3_filename)
-print("-------------> Converting mp4 to mp3...")
-mp4_to_mp3(mp4_filename=mp4_filename, mp3_filename=mp3_filename)
-print("-------------> Converting mp3 to text...")
-transcricao = mp3_to_text(mp3_filename=mp3_filename)
-print("-------------> Generating minutes...")
-openai_api_token = os.getenv("OPENAI_API_TOKEN")
-openai_client = OpenAI(api_key=openai_api_token)
+if uploaded_file is not None:
+    mp4_filename = uploaded_file.name
+    mp3_filename = uuid.uuid4().hex + ".mp3"
 
-system_prompt = "Você é um ótimo gerente de projetos com õtimas capacidades de gerar atas de reunião."
-user_prompt = f"""em redação de nível especializado, resuma as notas da reunião em um único parágrafo.
-Em seguida, esceva ua lista de cada um de seus pntos-chaves tratados na reunião.
-Por fim, liste as próxias etapas ou itens de ação sugeridos pelos palestrante, se houver.
-O texto transcrito da reunião segue abaixo:
-{transcricao}"""
+    st.text(mp3_filename)
 
-print("-------------> Result: ")
-print(gpt_request(openai_client, system_prompt, user_prompt))
+    st.text("Converting mp4 to mp3...")
+    print("-------------> Converting mp4 to mp3...")
+    mp4_to_mp3(mp4_filename=mp4_filename, mp3_filename=mp3_filename)
+
+    st.text("Converting mp3 to text...")
+    print("-------------> Converting mp3 to text...")
+    transcricao = mp3_to_text(mp3_filename=mp3_filename)
+
+    st.text("Generating minutes...")
+    print("-------------> Generating minutes...")
+    openai_api_token = os.getenv("OPENAI_API_TOKEN")
+    openai_client = OpenAI(api_key=openai_api_token)
+
+    system_prompt = "Você é um ótimo gerente de projetos com õtimas capacidades de gerar atas de reunião."
+    user_prompt = f"""em redação de nível especializado, resuma as notas da reunião em um único parágrafo.
+    Em seguida, esceva ua lista de cada um de seus pontos-chaves tratados na reunião.
+    Por fim, liste as próxias etapas ou itens de ação sugeridos pelos palestrante, se houver.
+    O texto transcrito da reunião segue abaixo:
+    {transcricao}"""
+
+    st.text("Result: ")
+    print("-------------> Result: ")
+    st.text(gpt_request(openai_client, system_prompt, user_prompt))
+    print(gpt_request(openai_client, system_prompt, user_prompt))
